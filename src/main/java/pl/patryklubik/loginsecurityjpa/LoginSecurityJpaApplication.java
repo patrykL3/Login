8f8c9f37ba678c5772ad08b4f7d1537d95953118
Patryk Łubik <patryk.lubik@wp.pl>
2021-06-09 19:53:26 +0200

    Skeleton

---
+++ src/main/java/pl/patryklubik/loginsecurityjpa/LoginSecurityJpaApplication.java
@@ -0,0 +1,13 @@
+package pl.patryklubik.loginsecurityjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSecurityJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSecurityJpaApplication.class, args);
+	}
+
+}
